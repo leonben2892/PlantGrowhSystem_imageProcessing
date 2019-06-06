2c1e6777918a147407d99b5c645f14bdab7c85f9
Leon Benjamin <leon.ben2892@gmail.com>
2019-06-06 11:37:02 +0300

    Added http get to recieve plant ID

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from PlantData import *
 import requests
 
-def https_post(plant_volume, plant_height):
-    r = requests.post("https://plantgrowthsystembackend.azurewebsites.net/Plant/UpdateSize", data = {'id': '5c9123439ec5fc4398bfef5b', 'Volume': plant_volume, 'Height': plant_height})
+# Https post
+def send_data_to_server(plant_id, plant_volume, plant_height):
+    r = requests.post("https://plantgrowthsystembackend.azurewebsites.net/Plant/UpdateSize", data = {'id': plant_id, 'Volume': plant_volume, 'Height': plant_height})
     print(r.status_code, r.reason)
     print(r.text[:300] + '...')
+
+# Https get
+def get_plant_id():
+    request_ip = '10.12.61.80'
+    r = requests.get("https://plantgrowthsystembackend.azurewebsites.net/Research/GetNewResearchByGrowthControlUnitIP?ip="+request_ip)
+    return r.content
 
 
 def main():
@@ -15,7 +22,9 @@ def main():
     cv2.imshow("{} Side".format(plant.plant_type), plant.side_image)
     cv2.imshow("{} Surface Area".format(plant.plant_type), plant.plant_area_image)
     
-    #https_post(plant.volume, plant.height)
+    # plant_id = get_plant_id()
+    # send_data_to_server(plant_id, plant.volume, plant.height)
+    
     cv2.waitKey(0)
 
 if __name__ == "__main__":
